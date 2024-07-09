d39853f264cc53cf3e2dcfeedf327f4e27f658dd
ipunganwar <ipung.anwar1@gmail.com>
2024-07-09 21:51:51 +0700

    fix: chore typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
+// import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs_mysql_tutorial',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
