f3ee3730315789cf0e77b67803ae6aa9fee3c91c
ipunganwar <ipung.anwar1@gmail.com>
2024-07-09 14:28:10 +0700

    chore: mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [DatabaseModule],
   controllers: [],
   providers: [],
 })
